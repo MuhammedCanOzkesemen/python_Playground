023c0b359ff82b4ad56e4c950b64dcd233fbf009
MuhammedOzkesemen <ozkemu_aehit@students.vizja.pl>
2025-10-04 13:38:07 +0200

    start.py
    day1 start (prints, math, logic)

---
+++ start.py
@@ -0,0 +1,31 @@
+# my first real code 
+# trying variables, prints and some basic logic 💪
+
+print("Hello World from Can\n")
+
+name = "Can"
+age = 21
+country = "Poland"
+print("My name is", name)
+print("I'm", age, "years old")
+print("I live in", country)
+
+print("\n Math part ")
+x = 10
+y = 4
+print("x =", x, "| y =", y)
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", round(x / y, 2))
+
+print("\n Logic part ")
+temperature = 18
+if temperature > 25:
+    print("Hot day ☀️")
+elif temperature > 15:
+    print("Nice weather 🌤️")
+else:
+    print("Cold ❄️")
+
+print("\n Done! ")
